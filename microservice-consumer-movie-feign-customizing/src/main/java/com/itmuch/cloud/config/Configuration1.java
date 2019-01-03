3688a64d7d8b39df3e895e66066f2a6dd0357e64
shenwenfang106 <shenwenfangsha@gmail.com>
2019-01-03 12:54:40 +0800

    Feign的日志配置

--- microservice-consumer-movie-feign-customizing/src/main/java/com/itmuch/cloud/config/Configuration1.java
+++ microservice-consumer-movie-feign-customizing/src/main/java/com/itmuch/cloud/config/Configuration1.java
@@ -1,6 +1,7 @@
 package com.itmuch.cloud.config;
 
 import feign.Contract;
+import feign.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,4 +11,10 @@ public class Configuration1 {
     public Contract feignContract(){
         return new feign.Contract.Default();
     }
+
+    @Bean
+    Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
+
 }
