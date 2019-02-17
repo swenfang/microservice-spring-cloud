8740e2e031df19f85e7797b8b1e5ddc581cf8a09
shenwenfang106 <shenwenfangsha@gmail.com>
2019-02-17 19:19:53 +0800

    Feign 对 Hystrix 的支持

---
+++ microservice-consumer-movice-feign-with-hystrix/src/main/java/com/itmuch/cloud/controller/MovieController.java
@@ -0,0 +1,27 @@
+package com.itmuch.cloud.controller;
+
+import com.itmuch.cloud.entity.User;
+import com.itmuch.cloud.feign.UserFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+  * @Author shenwenfang
+  * @Date 2018/12/16 19:46
+  * @Description: 
+  */
+@RestController
+public class MovieController {
+
+    // 调用
+    @Autowired
+    private UserFeignClient user;
+
+    @GetMapping("/movice/{id}")
+    public User findById(@PathVariable Long id){
+        return user.findById(id);
+    }
+
+}
